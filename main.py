97f76d234011802f12bbc4201f65be83f1ff8143
Simon Gokstorp <simon@gokstorp.se>
2019-03-19 12:23:53 +0000

    Add new example/test VTK functions

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Window(Frame):
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.master = master
-        self.init_window(cols=5, rows=5) # Use a 5x5 grid in the main frame
+        self.init_window(cols=4, rows=4) # Use a 5x5 grid in the main frame
 
         
 
@@ -38,23 +38,302 @@ class Window(Frame):
                 Label(self, text='').grid(column=col, row=row)
 
         # Create a button instance
-        quitButton = Button(self, text="Quit", command=self.client_exit, height=5, width=20)
+        quitButton = Button(self, text="Quit", command=self.client_exit, height=10, width=40)
         # Place the button in the window
-        quitButton.grid(column=4, row=4)
+        quitButton.grid(column=3, row=3)
 
         # Create a button instance
-        quitButton = Button(self, text="Cube", command=self.vtk_generate_cube, height=5, width=20)
+        quitButton = Button(self, text="Cube", command=self.vtk_generate_cube, height=10, width=40)
         # Place the button in the window
-        quitButton.grid(column=0, row=4)
+        quitButton.grid(column=0, row=3)
 
         # Create a button instance
-        quitButton = Button(self, text="Sphere", command=self.vtk_sphere_lighting, height=5, width=20)
+        quitButton = Button(self, text="Grid 2D", command=self.grid_2D, height=10, width=40)
         # Place the button in the window
-        quitButton.grid(column=1, row=4)
+        quitButton.grid(column=0, row=2)
+
+        # Create a button instance
+        quitButton = Button(self, text="Point Cloud", command=self.point_cloud, height=10, width=40)
+        # Place the button in the window
+        quitButton.grid(column=1, row=2)
+
+        # Create a button instance
+        quitButton = Button(self, text="Glyphs", command=self.glyphs_at_points, height=10, width=40)
+        # Place the button in the window
+        quitButton.grid(column=2, row=2)
+
+        # Create a button instance
+        quitButton = Button(self, text="Terrain 2D", command=self.terrain_surface_2D, height=10, width=40)
+        # Place the button in the window
+        quitButton.grid(column=3, row=2)
+        
+        # Create a button instance
+        quitButton = Button(self, text="Test", command=self.test, height=10, width=40)
+        # Place the button in the window
+        quitButton.grid(column=2, row=0)
 
 
     def client_exit(self):
         exit()
+
+    def test(self):
+        pass
+
+    def terrain_surface_2D(self):
+        # Produce rating distributions if they are not already stored
+        if len(self.rating_dist_by_genre) == 0:
+            print("Producing rating distributions")
+            self.rating_dist_by_genre = de.get_rating_dist_by_genre(self.movies, self.aggregate_ratings)
+        
+        genre_list = self.rating_dist_by_genre.keys()
+
+        # Set up labels for the X and Y axes
+        width = len(genre_list)
+        height = 9
+
+        # Data structures for labels
+        pd = vtk.vtkPolyData()
+        pts = vtk.vtkPoints()
+        verts = vtk.vtkCellArray()
+        label = vtk.vtkStringArray()
+        label.SetName('label')
+
+        # Y axis labels
+        for y in range(height):
+            pts.InsertNextPoint(-1, y, 0)
+            verts.InsertNextCell(1)
+            verts.InsertCellPoint(y)
+            orient.InsertNextValue(0)
+            label.InsertNextValue(str((y+1)*0.5))
+        # Add Y axis title
+        pts.InsertNextPoint(-2, (height-1)/2, 0)
+        verts.InsertNextCell(1)
+        verts.InsertCellPoint(height)
+        label.InsertNextValue("Scores")
+
+        # X axis labels
+        for x in range(width):
+            pts.InsertNextPoint(x, -1, 0)
+            verts.InsertNextCell(1)
+            verts.InsertCellPoint(x)
+            orient.InsertNextValue(0)
+            label.InsertNextValue(genre_list[x])
+        # Add X axis title
+        pts.InsertNextPoint((width-1)/2, -2, 0)
+        verts.InsertNextCell(1)
+        verts.InsertCellPoint(width)
+        label.InsertNextValue("Genre")
+
+        pd.SetPoints(pts)
+        pd.SetVerts(verts)
+        pd.GetPointData().AddArray(label)
+
+        hier = vtk.vtkPointSetToLabelHierarchy()
+        hier.SetInputData(pd)
+        hier.SetLabelArrayName('label')
+        hier.GetTextProperty().SetColor(0.0, 0.0, 0.0)
+
+        lmapper = vtk.vtkLabelPlacementMapper()
+        lmapper.SetInputConnection(hier.GetOutputPort())
+        # lmapper.SetShapeToRoundedRect()
+        # lmapper.SetBackgroundColor(1.0, 1.0, 0.7)
+        # lmapper.SetBackgroundOpacity(0.8)
+        # lmapper.SetMargin(3)
+
+        lactor = vtk.vtkActor2D()
+        lactor.SetMapper(lmapper)
+
+
+        colors = vtk.vtkNamedColors()
+        # Create the points in the grid
+        points = vtk.vtkPoints()
+        for x in range (width):
+            for y in range (height):
+                z = self.rating_dist_by_genre[genre_list[x]][y]
+                points.InsertNextPoint(x, y, z)
+
+        # Add the grid points to a polydata object
+        polydata = vtk.vtkPolyData()
+        polydata.SetPoints(points)
+
+        glyphFilter = vtk.vtkVertexGlyphFilter()
+        glyphFilter.SetInputData(polydata)
+        glyphFilter.Update()
+
+        # Create a mapper and actor
+        pointsMapper = vtk.vtkPolyDataMapper()
+        pointsMapper.SetInputConnection(glyphFilter.GetOutputPort())
+
+        pointsActor = vtk.vtkActor()
+        pointsActor.SetMapper(pointsMapper)
+        pointsActor.GetProperty().SetPointSize(3)
+        pointsActor.GetProperty().SetColor(colors.GetColor3d("Red"))
+
+        # Triangulate the grid points
+        delaunay = vtk.vtkDelaunay2D()
+        delaunay.SetInputData(polydata)
+        delaunay.Update()
+
+        # Create a mapper and actor
+        triangulatedMapper = vtk.vtkPolyDataMapper()
+        triangulatedMapper.SetInputConnection(delaunay.GetOutputPort())
+
+        triangulatedActor = vtk.vtkActor()
+        triangulatedActor.SetMapper(triangulatedMapper)
+
+        # Create a renderer, render window, and interactor
+        renderer = vtk.vtkRenderer()
+        renderWindow = vtk.vtkRenderWindow()
+        renderWindow.AddRenderer(renderer)
+        renderWindowInteractor = vtk.vtkRenderWindowInteractor()
+        renderWindowInteractor.SetRenderWindow(renderWindow)
+
+        # Add the actors to the scene
+        renderer.AddActor(lactor)
+        renderer.AddActor(pointsActor)
+        renderer.AddActor(triangulatedActor)
+        renderer.SetBackground(colors.GetColor3d("Green")) # Background color green
+
+        # Render and interact
+        renderWindow.Render()
+        renderWindowInteractor.Start()
+
+    def glyphs_at_points(self):
+        colors = vtk.vtkNamedColors()
+
+        points = vtk.vtkPoints()
+        points.InsertNextPoint(0,0,0)
+        points.InsertNextPoint(1,1,1)
+        points.InsertNextPoint(2,2,2)
+
+        polydata = vtk.vtkPolyData()
+        polydata.SetPoints(points)
+
+        # Create anything you want here, we will use a cube for the demo.
+        sphereSource = vtk.vtkSphereSource()
+
+        glyph3D = vtk.vtkGlyph3D()
+        glyph3D.SetSourceConnection(sphereSource.GetOutputPort())
+        glyph3D.SetInputData(polydata)
+        glyph3D.Update()
+
+        # Visualize
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInputConnection(glyph3D.GetOutputPort())
+
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
+
+        renderer = vtk.vtkRenderer()
+        renderWindow = vtk.vtkRenderWindow()
+        renderWindow.AddRenderer(renderer)
+        renderWindowInteractor = vtk.vtkRenderWindowInteractor()
+        renderWindowInteractor.SetRenderWindow(renderWindow)
+
+        renderer.AddActor(actor)
+        renderer.SetBackground(colors.GetColor3d("SlateGray")) # Background Slate Gray
+
+        renderWindow.Render()
+        renderWindowInteractor.Start()
+
+    def point_cloud(self):
+        colors = vtk.vtkNamedColors()
+
+        # create a rendering window and renderer
+        ren = vtk.vtkRenderer()
+        renWin = vtk.vtkRenderWindow()
+        renWin.AddRenderer(ren)
+
+        # create a renderwindowinteractor
+        iren = vtk.vtkRenderWindowInteractor()
+        iren.SetRenderWindow(renWin)
+
+        # create source
+        src = vtk.vtkPointSource()
+        src.SetCenter(0, 0, 0)
+        src.SetNumberOfPoints(50)
+        src.SetRadius(5)
+        src.Update()
+
+        # mapper
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInputConnection(src.GetOutputPort())
+
+        # actor
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
+        actor.GetProperty().SetColor(colors.GetColor3d('Yellow'))
+
+        actor.GetProperty().SetPointSize(5)
+
+        # Add axes
+        transform = vtk.vtkTransform()
+        transform.Translate(1.0, 0.0, 0.0)
+
+        axes = vtk.vtkAxesActor()
+        #  The axes are positioned with a user transform
+        axes.SetUserTransform(transform)
+
+        # properties of the axes labels can be set as follows
+        # this sets the x axis label to red
+        # axes.GetXAxisCaptionActor2D().GetCaptionTextProperty().SetColor(colors.GetColor3d("Red"));
+
+        # the actual text of the axis label can be changed:
+        axes.SetXAxisLabelText("test")
+        axes.SetYAxisLabelText("testY")
+        axes.SetZAxisLabelText("testZ")
+
+
+        # assign actors to the renderer
+        ren.AddActor(actor)
+        ren.AddActor(axes)
+        ren.SetBackground(colors.GetColor3d('RoyalBLue'))
+
+        # enable user interface interactor
+        iren.Initialize()
+        renWin.Render()
+        iren.Start()
+
+    def grid_2D(self):
+        """Generate a 2D grid in 3D space of 
+        """
+        # Provide some geometry.
+        xResolution = 10
+        yResolution = 10
+        aPlane = vtk.vtkPlaneSource()
+        aPlane.SetXResolution(xResolution)
+        aPlane.SetYResolution(yResolution)
+        size = xResolution * yResolution + 1
+
+        # Create cell data.
+        cellData = vtk.vtkFloatArray()
+        for i in range(0, xResolution * yResolution):
+            cellData.InsertNextValue(i)
+        aPlane.Update()  # Force an update so we can set cell data.
+        aPlane.GetOutput().GetCellData().SetScalars(cellData)
+
+
+        # Set up the actor and mapper.
+        mapper = vtk.vtkPolyDataMapper()
+        mapper.SetInputConnection(aPlane.GetOutputPort())
+        mapper.SetScalarModeToUseCellData()
+        mapper.SetScalarRange(0, size)
+
+        actor = vtk.vtkActor()
+        actor.SetMapper(mapper)
+        actor.GetProperty().EdgeVisibilityOn()
+
+        # Setup render window, renderer, and interactor.
+        renderer = vtk.vtkRenderer()
+        renderWindow = vtk.vtkRenderWindow()
+        renderWindow.AddRenderer(renderer)
+        renderWindowInteractor = vtk.vtkRenderWindowInteractor()
+        renderWindowInteractor.SetRenderWindow(renderWindow)
+
+        renderer.AddActor(actor)
+        renderer.SetBackground(colors.GetColor3d('SlateGray'))
+        renderWindow.Render()
+        renderWindowInteractor.Start()
 
     def vtk_generate_cube(self):
         # create polygonal cube geometry
@@ -92,46 +371,6 @@ class Window(Frame):
         renWin.Render()
 
         # begin mouse interaction
-        iren.Start()
-
-    def vtk_sphere_lighting(self):
-        # create a rendering window and renderer
-        ren = vtk.vtkRenderer()
-        renWin = vtk.vtkRenderWindow()
-        renWin.AddRenderer(ren)
-
-        # create a renderwindowinteractor
-        iren = vtk.vtkRenderWindowInteractor()
-        iren.SetRenderWindow(renWin)
-
-        # create source
-        source = vtk.vtkSphereSource()
-        source.SetCenter(0,0,0)
-        source.SetRadius(5.0)
-
-        # mapper
-        mapper = vtk.vtkPolyDataMapper()
-        if vtk.VTK_MAJOR_VERSION <= 5:
-            mapper.SetInput(source.GetOutput())
-        else:
-            mapper.SetInputConnection(source.GetOutputPort())
-
-        # actor
-        actor = vtk.vtkActor()
-        actor.SetMapper(mapper)
-        actor.GetProperty().SetColor(hot_pink)
-        actor.GetProperty().SetSpecularColor(1, 1, 1)
-        actor.GetProperty().SetSpecular(0.3)
-        actor.GetProperty().SetSpecularPower(20)
-        actor.GetProperty().SetAmbient(0.2)
-        actor.GetProperty().SetDiffuse(0.8)
-
-        # assign actor to the renderer
-        ren.AddActor(actor)
-
-        # enable user interface interactor
-        iren.Initialize()
-        renWin.Render()
         iren.Start()
 
 
